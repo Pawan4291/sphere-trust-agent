4903394a279cbb8ba6c52d447722f8d77e41a0a3
Pawan4291 <pawan429014@gmail.com>
2026-07-28 21:14:44 +0530

    fix: erro 500

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,6 +4,8 @@ import * as schema from "./schema";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
+  max: 1,
+  idleTimeoutMillis: 5000,
 });
 
 export const db = drizzle(pool, { schema });
